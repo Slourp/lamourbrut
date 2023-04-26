7ab04e030b6d3ae7f6d29bb09c4babf3a5503bb1
Thomas Chase <chasethomasparis@gmail.com>
2023-04-26 20:37:18 +0200

    (Shop): Responsive Section done

--- src/screens/Shop.jsx
+++ src/screens/Shop.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
-import Image from '../assets/shop/shop.png'
-import Headline from '../components/shared/Headline'
-import {headline2} from '../data/headlines'
-import Footer from '../layout/Footer'
+import React from 'react';
+import Image from '../assets/shop/shop.png';
+import Headline from '../components/shared/Headline';
+import {headline2} from '../data/headlines';
+import Footer from '../layout/Footer';
 
 const Products = () => {
   return (
-    <section className='w-full'>
-      
-    <div className="">
-  
-      <img
-        className="w-full h-[700px] object-cover"
-        src={Image}
-        style={{
-          zIndex: '-1',
-        }}
-        alt="E-SHOP Background"
+    <section style={{backgroundImage: `url(${Image})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+     
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
+        
+      <h1 
+      style={{fontFamily: 'Arial Black'}}
+      className="text-center  text-pink-500 text-shadow-lg text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+      Coming <span style={{fontFamily: 'times-new-roman'}} className="">Soon</span>
+    </h1>
+      </div>
+      <Headline
+        headlines={headline2}
+        backgroundColor='bg-black'
+        textColor='text-white'
+        textSize={32}
       />
-    </div>
+          <Footer />
 
-    <h2 style={{fontFamily: 'Arial Black'}}
-     className='absolute left-[50%] -translate-x-[50%] top-14 md:text-[30px]' style={{ zIndex: 100, fontSize: ' 9em', marginTop: '10rem', color: '#E45C96',  textShadow: '6px 6px 20px rgba(0, 0, 0, 0.6)' }} >COMING <span style={{fontFamily: 'times-new-roman'}}>SOON </span></h2>
-    <Headline
-    headlines={headline2}
-    backgroundColor='bg-black'
-    textColor='text-white'
-    textSize={32}
-  />
-    <Footer />
+    </section>
+  );
+};
 
-  </section>
-  )
-}
-
-export default Products
+export default Products;
