5d9be3971fbe7cb328db1bab765079cc2d9a9247
Thomas Chase <chasethomasparis@gmail.com>
2023-04-26 18:55:51 +0200

    feat(Backstage): Amelioration of Responsive

--- src/components/Home/Backstage.jsx
+++ src/components/Home/Backstage.jsx
@@ -8,39 +8,42 @@ const images = [Image1, Image2, Image3, Image4]
 
 const Backstage = () => {
   return (
-    <section className="bg-black p-20">
-      
-      <div className="container mx-auto">
+    <section className="bg-black p-2">
+      <div className="container mx-auto m-8">
         <h2 
-        style={{fontFamily: 'Arial Black'}}
-        className="text-white text-5xl md:text-6xl lg:text-5xl xl:text-[90px] 2xl:text-7xl">©
+          style={{fontFamily: 'Arial Black'}}
+          className="text-white text-5xl md:text-6xl lg:text-5xl xl:text-[90px] 2xl:text-7xl"
+        >
           Backstage
         </h2>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center col-12"
+             style={{flexDirection: 'column'}}
+        >
           {images.map((image, index) => (
             <div key={index} className="flex justify-center">
               <img
                 src={image}
                 alt=""
-                className="h-full object-cover backstage-img"
+                className="h-full object-cover backstage-img w-full"
               />
             </div>
           ))}
         </div>
         <p
-        style={{fontFamily: 'Arial Black'}} 
-        className='text-white mt-6'>BEHIND THE SCENE WITH L'AMOUR BRUT'S FAMILY</p>
+          style={{fontFamily: 'Arial Black'}} 
+          className='text-white mt-6'
+        >
+          BEHIND THE SCENE WITH L'AMOUR BRUT'S FAMILY
+        </p>
         <p
-        style={{fontFamily:'times-new-roman'}}
-         className="text-white mt-6 text-[25px]">
-           We like to think
-          that working with our friends and acquaintances allows us to
-          go further together. We met our photographers in school and
-          our models are our friends. Feel free to check out the ABOUT
-          US page to discover them.
+          style={{fontFamily:'times-new-roman'}}
+          className="text-white text-justify mt-4 md:mt-8 text-lg md:text-[25px]"
+        >
+          We like to think that working with our friends and acquaintances allows us to
+          go further together. We met our photographers in school and our models are our friends. 
+          Feel free to check out the ABOUT US page to discover them.
         </p>
       </div>
-     
     </section>
   )
 }
