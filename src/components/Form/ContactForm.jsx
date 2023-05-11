c0faee74bb8f70c73daa25c5075216ff725174b7
Thomas Chase <chasethomasparis@gmail.com>
2023-05-11 14:10:45 +0200

    fix:(CONTACT) added a form to the contact section and connected it to email.js

--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 function ContactForm() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const form = useRef()
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log('Name:', name)
-    console.log('Email:', email)
-    console.log('Message:', message)
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_q3ltw1t',
+        'template_vvq2wv6',
+        form.current,
+        'ydGYh6NeaKR8hWmrF'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+    if (sendEmail) {
+      document.location.href = '/'
+      alert(
+        `Votre message a bien été pris en compte et nous en vous remercions. Nous vous répondrons dans les plus brefs délais. L'Amour Brut`
+      )
+    }
   }
 
   return (
     <form
-      style={{fontFamily: 'arial-black'}}
-      onSubmit={handleSubmit}
+      ref={form}
+      onSubmit={sendEmail}
+      style={{ fontFamily: 'arial-black' }}
       className="max-w-lg mx-auto flex-col"
     >
       <div className="mb-4">
@@ -24,26 +42,28 @@ function ContactForm() {
         </label>
         <input
           type="text"
+          name="user_name"
           id="name"
           placeholder="Enter your name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
           required
-          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 focus:text-black"
+          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg text-black"
         />
       </div>
       <div className="mb-4">
-        <label htmlFor="email" className="block font-medium mb-2">
+        <label
+          name="user_email"
+          htmlFor="email"
+          className="block font-medium mb-2"
+        >
           Your mail
         </label>
         <input
           type="email"
           id="email"
           placeholder="Enter your mail"
-          value={email}
           onChange={(event) => setEmail(event.target.value)}
           required
-          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 focus:text-black"
+          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg text-black"
         />
       </div>
       <div className="mb-4">
@@ -51,16 +71,17 @@ function ContactForm() {
           Your Message
         </label>
         <textarea
+          name="message"
           id="message"
           placeholder="Enter your message"
-          value={message}
           onChange={(event) => setMessage(event.target.value)}
           required
-          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50 focus:text-black"
+          className="w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg w-[300px] lg:w-[500px] sm:w-[300px] px-4 py-2 rounded-lg text-black"
         />
       </div>
       <div className="flex justify-center absolute">
         <button
+          value="Send"
           type="submit"
           className=" px-4 py-2 bg-white text-black font-medium rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-opacity-50"
         >
