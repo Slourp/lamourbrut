fab899ded5d6d9fd572f7f2f0b5d6504af17cfaa
Thomas Chase <chasethomasparis@gmail.com>
2023-05-20 15:21:40 +0200

    fix:(BACKSTAGE + SPEOS CAROUSSEL) fixed eslint avertissments

--- src/components/Caroussel/CarousselBackstage.jsx
+++ src/components/Caroussel/CarousselBackstage.jsx
@@ -56,26 +56,25 @@ const CarousselBackstage = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const isMobile = window.innerWidth <= 768
+  const slideToShow = isMobile ? 1 : 3
+
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide
-      ? slides.length - 1
-      : currentIndex - 1
-    setCurrentIndex(newIndex)
+    const newIndex =
+      (currentIndex - 1 + slides.length) % slides.length
+    const visibleIndex = (newIndex + slideToShow - 1) % slides.length
+    setCurrentIndex(visibleIndex)
   }
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1
-    const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex)
+    const newIndex = (currentIndex + 1) % slides.length
+    const visibleIndex = newIndex
+    setCurrentIndex(visibleIndex)
   }
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex)
   }
-
-  const isMobile = window.innerWidth <= 768
-  const slideToShow = isMobile ? 1 : 3
 
   return (
     <div className="max-w-[1400px] h-[520px] w-full m-auto py-12 px-4 relative group">
@@ -86,11 +85,15 @@ const CarousselBackstage = () => {
       >
         {slides
           .slice(currentIndex, currentIndex + slideToShow)
-          .map((slide, slideIndex) => (
+          .map((slide) => (
             <div
-              key={slideIndex}
-              style={{ backgroundImage: `url(${slide.url})` }}
-              className="flex-1 rounded-2xl bg-center bg-cover"
+              key={slide.url}
+              style={{
+                backgroundImage: `url(${slide.url})`,
+                width: isMobile ? '100%' : '33%',
+                height: '420px',
+              }}
+              className="rounded-2xl bg-center bg-cover"
             />
           ))}
         {/* Left Arrow */}
@@ -104,8 +107,9 @@ const CarousselBackstage = () => {
       </div>
       <div className="flex top-4 justify-center py-2">
         {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
+          <button
+            type="button"
+            key={(slideIndex, slide)}
             onClick={() => goToSlide(slideIndex)}
             className={`text-2xl cursor-pointer ${
               slideIndex >= currentIndex &&
@@ -115,7 +119,7 @@ const CarousselBackstage = () => {
             }`}
           >
             <RxDotFilled />
-          </div>
+          </button>
         ))}
       </div>
     </div>
--- src/components/Caroussel/CarousselLf.jsx
+++ src/components/Caroussel/CarousselLf.jsx
@@ -36,41 +36,44 @@ const CarousselLf = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const isMobile = window.innerWidth <= 768
+  const slideToShow = isMobile ? 1 : 3
+
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide
-      ? slides.length - 1
-      : currentIndex - 1
-    setCurrentIndex(newIndex)
+    const newIndex =
+      (currentIndex - 1 + slides.length) % slides.length
+    const visibleIndex = (newIndex + slideToShow - 1) % slides.length
+    setCurrentIndex(visibleIndex)
   }
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1
-    const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex)
+    const newIndex = (currentIndex + 1) % slides.length
+    const visibleIndex = newIndex
+    setCurrentIndex(visibleIndex)
   }
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex)
   }
 
-  const isMobile = window.innerWidth <= 768
-  const slideToShow = isMobile ? 1 : 3
-
   return (
-    <div className="max-w-[1400px] h-[520px] w-full m-auto py-16 px-4 relative group">
+    <div className="max-w-[1400px] h-[420px] w-full m-auto py-12 px-4 relative group">
       <div
-        className={`w-full h-[320px] rounded-2xl bg-center bg-cover duration-500 ${
+        className={`w-full h-[330px] rounded-2xl bg-center bg-cover duration-500 ${
           isMobile ? 'flex flex-col' : 'flex flex-row'
         } gap-4`}
       >
         {slides
           .slice(currentIndex, currentIndex + slideToShow)
-          .map((slide, slideIndex) => (
+          .map((slide) => (
             <div
-              key={slideIndex}
-              style={{ backgroundImage: `url(${slide.url})` }}
-              className="flex-1 rounded-2xl bg-center bg-cover"
+              key={slide.url}
+              style={{
+                backgroundImage: `url(${slide.url})`,
+                width: isMobile ? '100%' : '33%',
+                height: '320px',
+              }}
+              className="rounded-2xl bg-center bg-cover"
             />
           ))}
         {/* Left Arrow */}
@@ -84,8 +87,9 @@ const CarousselLf = () => {
       </div>
       <div className="flex top-4 justify-center py-2">
         {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
+          <button
+            type="button"
+            key={(slideIndex, slide)}
             onClick={() => goToSlide(slideIndex)}
             className={`text-2xl cursor-pointer ${
               slideIndex >= currentIndex &&
@@ -95,7 +99,7 @@ const CarousselLf = () => {
             }`}
           >
             <RxDotFilled />
-          </div>
+          </button>
         ))}
       </div>
     </div>
