c214420ec96876ee1ace900f23f89d21c2ab6e4e
Thomas Chase <chasethomasparis@gmail.com>
2023-06-13 09:40:12 +0200

    feat:(ProductsDetails) created a file for to display productsdetails after cliqued in pictures products in general products

--- src/shopify/components/Products.jsx
+++ src/shopify/components/Products.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Client from 'shopify-buy'
 import Basket from './Basket'
+import ProductsDetails from './ProductsDetails'
 
 const Products = () => {
   const [products, setProducts] = useState([])
@@ -91,6 +92,16 @@ const Products = () => {
     </li>
   )
 
+  if (selectedProduct) {
+    return (
+      <ProductsDetails
+        product={selectedProduct}
+        setSelectedProduct={setSelectedProduct}
+        handleAddToCart={handleAddToCart}
+      />
+    )
+  }
+
   return (
     <div className="flex flex-col">
       <ul className="mt-[150px] flex max-w-[1200px] mx-auto gap-5">
@@ -108,18 +119,12 @@ const Products = () => {
             )}
             {product.images && product.images.length > 0 && (
               <img
-                className="w-[300px]"
+                className="w-[300px] h-[300px] object-cover"
                 src={product.images[0].src}
                 alt={product.title}
                 onClick={() => handleProductClick(product)}
               />
             )}
-            {selectedProduct === product && (
-              <p className="mt-3 text-gray-700">
-                {product.description}
-              </p>
-            )}
-
             {product.variants && product.variants.length > 0 && (
               <select className="mt-3 p-2 border border-gray-300 rounded">
                 {product.variants.map((variant) => (
@@ -144,39 +149,6 @@ const Products = () => {
         ))}
       </ul>
 
-      {selectedProduct && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="bg-white p-5 w-[1200px] h-[600px]">
-            <h3 className="text-lg font-semibold">
-              {selectedProduct.title}
-            </h3>
-            {selectedProduct.images &&
-              selectedProduct.images.length > 0 && (
-                <img
-                  className="w-[300px]"
-                  src={selectedProduct.images[0].src}
-                  alt={selectedProduct.title}
-                />
-              )}
-            {selectedProduct.variants &&
-              selectedProduct.variants.length > 0 && (
-                <p className="text-gray-600">
-                  Prix : {selectedProduct.variants[0].price.amount}{' '}
-                  {selectedProduct.variants[0].price.currencyCode}
-                </p>
-              )}
-            <p className="mt-3 text-gray-700">
-              {selectedProduct.description}
-            </p>
-            <button
-              className="mt-3 px-4 py-2 bg-black text-white font-semibold rounded hover:bg-blue-600"
-              onClick={() => setSelectedProduct(null)}
-            >
-              Fermer
-            </button>
-          </div>
-        </div>
-      )}
       <Basket cartItems={cartItems} CartItem={CartItem} />
     </div>
   )
---
+++ src/shopify/components/ProductsDetails.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { HiArrowLeft } from 'react-icons/hi'
+
+const ProductsDetails = ({ product }) => {
+  return (
+    <div className="mt-[150px]">
+      <div className="flex flex-col">
+        <div className="flex justify-around items-center">
+          {product.images && product.images.length > 0 && (
+            <img
+              className="w-[400px]"
+              src={product.images[0].src}
+              alt={product.title}
+            />
+          )}
+          <div>
+            <h3>{product.title}</h3>
+
+            {product.variants && product.variants.length > 0 && (
+              <p>
+                {product.variants[0].price.amount}{' '}
+                {product.variants[0].price.currencyCode}
+              </p>
+            )}
+            {product.description && (
+              <p className="max-w-[600px] leading-9">
+                {product.description}
+              </p>
+            )}
+            {/* Ajoutez d'autres informations du produit ici */}
+            <button
+              type="button"
+              onClick={() => {
+                window.location.href = '/test'
+              }}
+              className="text-black flex items-center text-[24px] font-extrabold border-[6px] border-black uppercase p-1 px-5 mt-12"
+            >
+              <HiArrowLeft className="mr-4" /> Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsDetails
